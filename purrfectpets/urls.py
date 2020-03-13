61e4d84f75887a214edef53435c39ecbd5b3b5da
Robert McDermid <2391520m@student.gla.ac.uk>
2020-03-13 00:12:00 +0000

    Urls

--- purrfectpets/urls.py
+++ purrfectpets/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from purrfect_pets import views
 
 urlpatterns = [
+    path('', views.home, name = 'home'),
+    path('purrfect_pets/', include('purrfect_pets.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
