f6c6b6081ab46e64e169c507892478e9af250d27
Henry Speir <2297552s@student.gla.ac.uk>
2020-03-19 12:50:33 +0000

    Models: first attempt

---
+++ purrfectpets_project/.ipynb_checkpoints/urls-checkpoint.py
@@ -0,0 +1,32 @@
+
+from django.contrib import admin
+from django.urls import path
+from django.urls import include
+from purrfectpets_project import views
+from django.conf import settings
+
+
+app_name = 'purrfectpets'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('contact_us/', views.contact_us, name = 'contact_us'),
+    path('about_us/', views.about_us, name="about_us"),
+    path('popular_pets/', views.popular_pets, name="popular_pets"),
+    path('categories/', views.categories, name="categories"),
+    path('dogs/', views.dogs, name="dogs"),
+    path('cats/', views.cats, name="cats"),
+    path('fish/', views.fish, name="fish"),
+    path('reptiles/', views.reptiles, name="reptiles"),
+    path('rodents/', views.rodents, name="rodents"),
+    path('other/', views.other, name="other"),
+    path('sign_up/', views.sign_up, name='sign_up'), 
+	path('login/', views.user_login, name='login'),
+	path('my_account/', views.my_account, name='my_account'),
+	path('my_pets/', views.my_pets, name='my_pets'),
+	path('login/my_account/my_pets/<slug:pet_name>/', views.pet_page, name = 'pet_page'),
+	path('login/my_account/my_pets/<slug:pet_name>/add_picture/', views.add_picture, name = 'add_picture'),
+	path('login/my_account/add_pet/', views.add_pet, name = 'add_pet'),
+	path('restricted/', views.restricted, name='restricted'),
+	path('logout/', views.user_logout, name = 'logout'),
+]
--- purrfectpets_project/models.py
+++ purrfectpets_project/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    email = models.CharField(unique=True,max_length=100)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Pet(models.Model):
+    owner = models.ForeignKey(UserProfile,on_delete=models.CASCADE, parent_link=True, related_name="owner")
+
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    animalType = models.CharField(max_length=100)
+    bio = models.TextField()
+    photos = []
+
+    def addPhoto(self, image):
+        photos = photos + [models.ImageField()]
+    
+    awwSenders = models.ManyToManyField(UserProfile, related_name="awwSenders")
+    awwCount = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Comment(models.Model):
+    commentMaker = models.ForeignKey(UserProfile,on_delete = models.CASCADE,parent_link=True)
+    commentAbout = models.ForeignKey(Pet,on_delete = models.CASCADE, parent_link=True)
+
+    timeDate = models.DateTimeField()
+
+    comment = models.TextField()
