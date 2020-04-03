a64c4327db5af8e3711c6e1517aa92a8b00db383
Ryan McGuigan <2388311M@student.gla.ac.uk>
2020-04-03 01:35:47 +0100

    Added some simple tests in tests.py

--- purrfectpets_project/tests.py
+++ purrfectpets_project/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from purrfectpets_project.models import Category
+from purrfectpets_project.views import home
 
-# Create your tests here.
+
+
+class Test_home_view(TestCase):
+
+    def test_list_url_accessible(self):
+        response = self.client.get(reverse('home'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_home_correct_template(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'purrfectpets_project/home.html')
+
+"""
+    def test_home_with_no_most_viewed_categories(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'There are no Categories up yet!')
+        self.assertEquals(response.context['categories'], None)
+
+"""
+
+"""
+class test_categories_view(TestCase):
+
+     def test_home_correct_template(self):
+        response = self.client.get(reverse('categories'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'purrfectpets_project/categories.html')
+"""
+    
+
+
